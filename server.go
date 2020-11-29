693d90297ca23c1e88a2634b8faec3176c35aa91
Freek <freek.van.dooren@loopseducation.com>
2020-11-29 21:40:20 +0100

    Logging to file for server events

--- server.go
+++ server.go
@@ -17,6 +17,10 @@ import (
 )
 
 func main() {
+	ensureLogsDirectory()
+	serverLog := createServerLogFile()
+	defer close(serverLog)
+	log.SetOutput(io.MultiWriter(os.Stdout, serverLog))
 	memories, err := history.Setup("data")
 	if err != nil {
 		log.Fatal(err)
@@ -26,24 +30,40 @@ func main() {
 		log.Fatal(err)
 	}
 	logWriter, logFile := setupHttpLogWriter()
-	defer func() {
-		err := logFile.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	defer close(logFile)
 
 	http.ListenAndServe(":8080", handlers.LoggingHandler(logWriter, setupRouter(memories, feedbackMessages)))
 }
 
-func setupHttpLogWriter() (io.Writer, *os.File) {
+func ensureLogsDirectory() {
 	_, err := os.Stat("logs")
 	if os.IsNotExist(err) {
 		err := os.Mkdir("logs", 0755)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 	}
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createServerLogFile() *os.File {
+	serverLog, err := os.OpenFile("logs/server.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	return serverLog
+}
+
+func close(logFile *os.File) {
+	err := logFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupHttpLogWriter() (io.Writer, *os.File) {
 	logFile, err := os.OpenFile("logs/http.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if err != nil {
 		log.Fatal(err)
@@ -58,26 +78,26 @@ func setupRouter(memories history.Service, feedbackMessages *messages.Service) *
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("No can do"))
 	})
-	r.HandleFunc("/", HomeHandler).Methods(http.MethodGet)
-	r.HandleFunc("/history", HistoryHandler(memories)).Methods(http.MethodGet)
-	r.HandleFunc("/primes/{number:[0-9]+}", PrimeHandler(memories, feedbackMessages)).Methods(http.MethodGet)
-	r.HandleFunc("/messages", GETMessagesHandler(feedbackMessages)).Methods(http.MethodGet)
-	r.HandleFunc("/messages", POSTMessagesHandler(feedbackMessages)).Methods(http.MethodPost)
+	r.HandleFunc("/", homeHandler).Methods(http.MethodGet)
+	r.HandleFunc("/history", historyHandler(memories)).Methods(http.MethodGet)
+	r.HandleFunc("/primes/{number:[0-9]+}", primeHandler(memories, feedbackMessages)).Methods(http.MethodGet)
+	r.HandleFunc("/messages", feedbackMessagesGETHandler(feedbackMessages)).Methods(http.MethodGet)
+	r.HandleFunc("/messages", feedbackMessagesPOSTHandler(feedbackMessages)).Methods(http.MethodPost)
 	return r
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Home!")
 }
 
-func PrimeHandler(memories history.Service, feedbackMessages *messages.Service) func(w http.ResponseWriter, r *http.Request) {
+func primeHandler(memories history.Service, feedbackMessages *messages.Service) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		potentialNumber := vars["number"]
 		number, err := strconv.Atoi(potentialNumber)
 		if err != nil {
-			fmt.Println(potentialNumber, "is not an integer.")
+			log.Println(potentialNumber, "is not an integer.")
 			http.Error(w, fmt.Sprintf("Not an integer: %s", potentialNumber), http.StatusBadRequest)
 			return
 		}
@@ -86,13 +106,13 @@ func PrimeHandler(memories history.Service, feedbackMessages *messages.Service) 
 	}
 }
 
-func HistoryHandler(memories history.Service) func(http.ResponseWriter, *http.Request) {
+func historyHandler(memories history.Service) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		sendAsJSONResponse(w, memories.ToHistoryResponse())
 	}
 }
 
-func GETMessagesHandler(feedbackMessages *messages.Service) func(http.ResponseWriter, *http.Request) {
+func feedbackMessagesGETHandler(feedbackMessages *messages.Service) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		sendAsJSONResponse(w, feedbackMessages.Get())
 	}
@@ -101,7 +121,7 @@ func GETMessagesHandler(feedbackMessages *messages.Service) func(http.ResponseWr
 /*
   Endpoint should have some security... We wouldn't want just anyone updating this.
 */
-func POSTMessagesHandler(feedbackMessages *messages.Service) func(http.ResponseWriter, *http.Request) {
+func feedbackMessagesPOSTHandler(feedbackMessages *messages.Service) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var messages responses.Messages
 		err := json.NewDecoder(r.Body).Decode(&messages)
